5679fc1905b24d09aa28d25af9762c4a9ccb322f
ManarAbdelkarim <manarbdelkarim434@gmail.com>
2021-03-01 16:50:40 +0200

    adding sign up and sign in

--- js/App.js
+++ js/App.js
@@ -1,14 +1,16 @@
 let table = document.getElementById('categoryContainer');
-
-table.addEventListener('click',changeColor);
-
-function changeColor(event){
+let dropdown = document.getElementById('redirectToCategory');
+table.addEventListener('click',redirectToCategory);
+dropdown.addEventListener('click',redirectToCategory);
+function redirectToCategory(event){
     debugger;
-    if(event.target.tagName.toLowerCase() === 'img'){
+    if(event.target.tagName.toLowerCase() === 'img' || event.target.tagName.toLowerCase() === 'a'){
     let id = event.target.id;
    
     localStorage.setItem('id',id);
     window.location="../category.html";
     }   
 }
+
+
 
---
+++ js/user.js
@@ -0,0 +1,18 @@
+let User = {
+    table:[]
+ };
+ 
+
+
+
+ function User(name,choicesArr=[],answersArr=[]) {
+    this.category = category;
+    this.questionsArr = questionsArr;
+    this.choicesArr = choicesArr;
+    this.answersArr = answersArr;
+    Quiz.all.push(this);
+    }
+    Quiz.all=[];
+    
+    let quiz = new Quiz
+ obj.table.push({id: 1, square:2});
