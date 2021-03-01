65fd6df76dc279c4c5ffecbd82a0bea920443486
ManarAbdelkarim <manarbdelkarim434@gmail.com>
2021-03-01 19:10:23 +0200

    add users sign up

--- js/user.js
+++ js/user.js
@@ -1,18 +1,51 @@
-let User = {
-    table:[]
- };
+let btnSignUp = document.getElementById('signup');
+let elements = document.getElementById("signUpForm").elements;
+const PASSWORD = document.getElementById("newPassword").elements;
+const CONFIRM$PASSWORD = document.getElementById("newPassword").elements;
  
 
 
 
- function User(name,choicesArr=[],answersArr=[]) {
-    this.category = category;
+ function User(name,password,email,userQuizzes=[]) {
     this.questionsArr = questionsArr;
     this.choicesArr = choicesArr;
     this.answersArr = answersArr;
-    Quiz.all.push(this);
+    User.all.push(this);
     }
-    Quiz.all=[];
+    User.all=[];
     
     let quiz = new Quiz
- obj.table.push({id: 1, square:2});
+
+
+    btnSignUp.addEventListener('click',getherUserInformation);
+    
+    User.prototype.getherUserInformation = function(event){
+      event.preventDefault();
+      debugger;
+      let error = document.getElementById('error');
+      let newUserName = document.getElementById('newUserName');
+      let newEmail = document.getElementById('newEmail');
+      if (PASSWORD !== CONFIRM$PASSWORD) {
+         error.textContent = "The password doesn't match";
+         break;
+      }
+      else {
+         for (let i = 0; i < User.all.length; i++) {
+           
+            if (newUserName.value === User.all[i].name) {
+               error.textContent = "The username is taken";
+               break;
+               
+            }
+
+           else if (newEmail.textContent === User.all[i].email) {
+               error.textContent = "The email is used before";
+               break;
+            }
+         }
+      }
+
+      alert('you can reach here');
+   
+
+    }
