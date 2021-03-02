b6065f3987e3fcc0140c9970e01c9e425d8035fe
ManarAbdelkarim <manarbdelkarim434@gmail.com>
2021-03-02 19:05:15 +0200

    fix sign up

--- js/user.js
+++ js/user.js
@@ -1,53 +1,136 @@
 'use strict'
 let btnSignUp = document.getElementById('signup');
+let btnSignIn = document.getElementsByClassName('signin')[0];
 let elements = document.getElementById("signUpForm").elements;
-const PASSWORD = document.getElementById("newPassword").elements;
-const CONFIRM$PASSWORD = document.getElementById("newPassword").elements;
- 
+let btnHome = document.getElementById("redirectToHome");
+let h1SignIn = document.getElementsByClassName("h1Sign2")[0];
+let h1SignUp = document.getElementsByClassName("h1Sign1")[0];
+let userName = localStorage.getItem('userName');
+let btnShowSignIn = document.getElementById("signIn");
+let btnShowSignUp = document.getElementById("signUp");
+
+btnHome.addEventListener('click',redirectToHome);
+let signUpCounter ;
 
 
 
- function User(name,password,email,arrUserQuizzes=[]) {
-    this.arrUserQuizzes = arrUserQuizzes;
-    this.name = name;
-    this.password = password;
-    this.email = email;
-    User.all.push(this);
-    }
-    User.all=[];
-    
-    let user = new User;
+h1SignUp.addEventListener('click',showSignUp);
+
+function showSignUp(event) {
+   
+   btnShowSignIn.style.display = "none";
+   btnShowSignUp.style.display ="block";
+
+}
 
 
-    btnSignUp.addEventListener('click',getherUserInformation);
-    
-    User.prototype.getherUserInformation = function(event){
-      event.preventDefault();
+h1SignIn.addEventListener('click',showSignIn);
+
+function showSignIn(event) {
+   debugger;
+  event.preventDefault();
+  btnShowSignUp.style.display ="none";
+  btnShowSignIn.style.display = "block";
+  
+
+}
+
+// take the the previous number from localStorage 
+function NoOfUsers() {
+   if (localStorage.getItem("SignUpCounter") === null) {
+      localStorage.setItem('SignUpCounter','0');
+      signUpCounter = 0;
+}
+else{
+   signUpCounter = localStorage.getItem("SignUpCounter");
+   signUpCounter = parseInt(signUpCounter);
+}
+}
+
+NoOfUsers();
+
+
+function redirectToHome(event){
+   event.preventDefault();
+   window.location = "../index.html";
+}
+   class User {
+   constructor(Id , name, password, email) {
+      this.Id = Id;
+      this.arrUserQuizzes = [];
+      this.name = name;
+      this.password = password;
+      this.email = email;
+      User.all.push(this);
+   }
+   getherUserInformation() {
       debugger;
+      if (localStorage.getItem("userInfo") !== null) {
+         User.all  = localStorage.getItem('userInfo');
+         User.all = JSON.parse(User.all);
+         console.log(User.all);
+      }
       let error = document.getElementById('error');
       let newUserName = document.getElementById('newUserName');
       let newEmail = document.getElementById('newEmail');
-      if (PASSWORD !== CONFIRM$PASSWORD) {
+      const PASSWORD = document.getElementById("newPassword");
+      const CONFIRM$PASSWORD = document.getElementById("confirmPassword");
+      let hidden = document.getElementById('hide');
+      let showing = document.getElementById('show');
+      if (PASSWORD.value !== CONFIRM$PASSWORD.value) {
          error.textContent = "The password doesn't match";
          return;
       }
       else {
          for (let i = 0; i < User.all.length; i++) {
-           
+
             if (newUserName.value === User.all[i].name) {
                error.textContent = "The username is taken";
                return;
-               
+
             }
 
-           else if (newEmail.textContent === User.all[i].email) {
+
+            else if (newEmail.value === User.all[i].email) {
                error.textContent = "The email is used before";
                return;
             }
          }
       }
 
-      alert('you can reach here');
-   
+      user.name = newUserName.value;
+      user.Id = signUpCounter;
+      user.password = PASSWORD.value;
+      user.email = newEmail.value;
+      // User.all.push(user);
+      console.log(user.all);
+      User.all.push(user);
+      signUpCounter += 1;
+      localStorage.setItem('SignUpCounter', signUpCounter);
+      localStorage.setItem('userInfo',JSON.stringify(User.all));
+      hidden.style.display = 'none';
+      showing.style.display = 'block';
 
+   }
+}
+   User.all=[];
+    
+   let user = new User;
+
+
+
+
+    btnSignUp.addEventListener('click',handelEvent);
+    
+
+    function handelEvent(event){
+      event.preventDefault();
+      user.getherUserInformation();
     }
+
+  
+
+
+
+
+  
